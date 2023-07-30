4ed34452df710c72953fa468b47f96e12a72fc78
Jordan Thayer <jtthayer@sep.com>
2023-07-30 10:43:13 -0400

    Reproduce VADER's demo code

---
+++ nlp/main.py
@@ -0,0 +1,94 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+# Reproduces the demo code packaged with VaderSentmiment
+if __name__ == "__main__":
+    sentences = ["VADER is smart, handsome, and funny.",  # positive sentence example
+                 "VADER is smart, handsome, and funny!",
+                 # punctuation emphasis handled correctly (sentiment intensity adjusted)
+                 "VADER is very smart, handsome, and funny.",
+                 # booster words handled correctly (sentiment intensity adjusted)
+                 "VADER is VERY SMART, handsome, and FUNNY.",  # emphasis for ALLCAPS handled
+                 "VADER is VERY SMART, handsome, and FUNNY!!!",
+                 # combination of signals - VADER appropriately adjusts intensity
+                 "VADER is VERY SMART, uber handsome, and FRIGGIN FUNNY!!!",
+                 # booster words & punctuation make this close to ceiling for score
+                 "VADER is not smart, handsome, nor funny.",  # negation sentence example
+                 "The book was good.",  # positive sentence
+                 "At least it isn't a horrible book.",  # negated negative sentence with contraction
+                 "The book was only kind of good.",
+                 # qualified positive sentence is handled correctly (intensity adjusted)
+                 "The plot was good, but the characters are uncompelling and the dialog is not great.",
+                 # mixed negation sentence
+                 "Today SUX!",  # negative slang with capitalization emphasis
+                 "Today only kinda sux! But I'll get by, lol",
+                 # mixed sentiment example with slang and constrastive conjunction "but"
+                 "Make sure you :) or :D today!",  # emoticons handled
+                 "Catch utf-8 emoji such as 💘 and 💋 and 😁",  # emojis handled
+                 "Not bad at all"  # Capitalized negation
+                 ]
+
+    analyzer = SentimentIntensityAnalyzer()
+
+    print("----------------------------------------------------")
+    print(" - Analyze typical example cases, including handling of:")
+    print("  -- negations")
+    print("  -- punctuation emphasis & punctuation flooding")
+    print("  -- word-shape as emphasis (capitalization difference)")
+    print("  -- degree modifiers (intensifiers such as 'very' and dampeners such as 'kind of')")
+    print("  -- slang words as modifiers such as 'uber' or 'friggin' or 'kinda'")
+    print("  -- contrastive conjunction 'but' indicating a shift in sentiment; sentiment of later text is dominant")
+    print("  -- use of contractions as negations")
+    print("  -- sentiment laden emoticons such as :) and :D")
+    print("  -- utf-8 encoded emojis such as 💘 and 💋 and 😁")
+    print("  -- sentiment laden slang words (e.g., 'sux')")
+    print("  -- sentiment laden initialisms and acronyms (for example: 'lol') \n")
+    for sentence in sentences:
+        vs = analyzer.polarity_scores(sentence)
+        print("{:-<65} {}".format(sentence, str(vs)))
+    print("----------------------------------------------------")
+    print(" - About the scoring: ")
+    print("""  -- The 'compound' score is computed by summing the valence scores of each word in the lexicon, adjusted
+     according to the rules, and then normalized to be between -1 (most extreme negative) and +1 (most extreme positive).
+     This is the most useful metric if you want a single unidimensional measure of sentiment for a given sentence.
+     Calling it a 'normalized, weighted composite score' is accurate.""")
+    print("""  -- The 'pos', 'neu', and 'neg' scores are ratios for proportions of text that fall in each category (so these
+     should all add up to be 1... or close to it with float operation).  These are the most useful metrics if
+     you want multidimensional measures of sentiment for a given sentence.""")
+    print("----------------------------------------------------")
+
+    # input("\nPress Enter to continue the demo...\n")  # for DEMO purposes...
+
+    tricky_sentences = ["Sentiment analysis has never been good.",
+                        "Sentiment analysis has never been this good!",
+                        "Most automated sentiment analysis tools are shit.",
+                        "With VADER, sentiment analysis is the shit!",
+                        "Other sentiment analysis tools can be quite bad.",
+                        "On the other hand, VADER is quite bad ass",
+                        "VADER is such a badass!",  # slang with punctuation emphasis
+                        "Without a doubt, excellent idea.",
+                        "Roger Dodger is one of the most compelling variations on this theme.",
+                        "Roger Dodger is at least compelling as a variation on the theme.",
+                        "Roger Dodger is one of the least compelling variations on this theme.",
+                        "Not such a badass after all.",  # Capitalized negation with slang
+                        "Without a doubt, an excellent idea."  # "without {any} doubt" as negation
+                        ]
+    print("----------------------------------------------------")
+    print(" - Analyze examples of tricky sentences that cause trouble to other sentiment analysis tools.")
+    print("  -- special case idioms - e.g., 'never good' vs 'never this good', or 'bad' vs 'bad ass'.")
+    print("  -- special uses of 'least' as negation versus comparison \n")
+    for sentence in tricky_sentences:
+        vs = analyzer.polarity_scores(sentence)
+        print("{:-<69} {}".format(sentence, str(vs)))
+    print("----------------------------------------------------")
+
+    # input("\nPress Enter to continue the demo...\n")  # for DEMO purposes...
+
+    print("----------------------------------------------------")
+    print(
+        " - VADER works best when analysis is done at the sentence level (but it can work on single words or entire novels).")
+    paragraph = "It was one of the worst movies I've seen, despite good reviews. Unbelievably bad acting!! Poor direction. VERY poor production. The movie was bad. Very bad movie. VERY BAD movie!"
+    print("  -- For example, given the following paragraph text from a hypothetical movie review:\n\t'{}'".format(
+        paragraph))
+    print(
+        "  -- You could use NLTK to break the paragraph into sentence tokens for VADER, then average the results for the paragraph like this: \n")
+    
