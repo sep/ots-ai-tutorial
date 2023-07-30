6e4c4d08788b41a8b8cae9b285628f3bc81b472a
Jordan Thayer <jtthayer@sep.com>
2023-07-30 11:01:55 -0400

    Add nltk to the set of things we download and setup

--- nlp/setup.sh
+++ nlp/setup.sh
@@ -2,7 +2,9 @@
 # Script for setting up the python environment for this tutorial
 python3 -m venv venv
 source ./venv/bin/activate
-pip3 install vaderSentiment
+pip3 install vaderSentiment nltk
+#TODO: Figure out exactly which nltk data we'll want and download just that!
+python3 -m nltk.downloader all
 mkdir data
 pushd data
 wget https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
