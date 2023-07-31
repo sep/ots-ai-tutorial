1cd7e33b1418725b0a578108b74e842f647e6096
Jordan Thayer <jtthayer@sep.com>
2023-07-31 08:24:23 -0400

    Can now load all of the maps from Nathan's BG2 set

--- planning/deterministicPlanning.py
+++ planning/deterministicPlanning.py
@@ -1,11 +1,14 @@
 from pathfinding.core.diagonal_movement import DiagonalMovement
 import os
 
-TEST_PATH= os.path.join("data", "maps", "AR0011SR.map")
+MAP_ROOT= os.path.join("data", "maps")
+TEST_PATH= os.path.join(MAP_ROOT, "AR0011SR.map")
 OCTILE_MOTION = 'type octile'
 HEIGHT = 'height'
 WIDTH = 'width'
 MAP = 'map'
+FREE = '.'
+OCCLUDED = '@'
 
 class Map():
     
@@ -24,8 +27,55 @@ class Map():
             else:
                 print("Saw unexpected motion model:", lines[0])
                 raise ValueError
-            print(lines[0:4])
+            height = -1
+            width = -1
+            dimLine = lines[1].split()
+            if dimLine[0] == HEIGHT:
+                height = int(dimLine[1])
+            elif dimLine[0] == WIDTH:
+                width = int(dimLine[1])
+            else:
+                print("Expected to find height or width, found:", lines[1])
+                raise ValueError
+            dimLine = lines[2].split()
+            if dimLine[0] == HEIGHT:
+                height = int(dimLine[1])
+            elif dimLine[0] == WIDTH:
+                width = int(dimLine[1])
+            else:
+                print("Expected to find height or width, found:", lines[1])
+                raise ValueError
+            if not (lines[3] == MAP):
+                print("Not parsing a map, instead reading a:", lines[3])
+                raise ValueError
+            grid = []
+            for mapLine in lines[4:]:
+                lineAr = []
+                for char in mapLine:
+                    if char == FREE:
+                      lineAr.append(False)
+                    elif char == OCCLUDED:
+                      lineAr.append(True)
+                    else:
+                      print("Saw an unexpected map glyph while parsing", char)
+                      raise ValueError
+                if not (len(lineAr) == width):
+                      print ("Map line had a bad size {}, expected {}".format(len(lineAr), width))
+                      raise ValueError
+                grid.append(lineAr)
+            if not (len(grid) == height):
+                      print ("Map has wrong height. Got {} but expected {}".format(len(grid), height))
+                      raise ValueError
+            newMap.occlusion = grid
+            return newMap
+
+
+def checkAllMapsLoadable(rootDir):
+    for fname in os.listdir(rootDir):
+        mapPath = os.path.join(rootDir, fname)
+        Map.loadMap(mapPath)
+    return True
             
 
 if __name__ == "__main__":
-    Map.loadMap(TEST_PATH)
+    checkAllMapsLoadable(MAP_ROOT)
