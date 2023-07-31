5822918ef351a77fa7ed7f80752ac035e5c7df18
Jordan Thayer <jtthayer@sep.com>
2023-07-31 08:06:20 -0400

    work on parsing maps

--- planning/pathfinding.py
+++ planning/pathfinding.py
@@ -1,4 +1,31 @@
+from pathfinding.core.diagonal_movement import DiagonalMovement
 import os
 
+TEST_PATH= os.path.join("data", "maps", "AR0011SR.map")
+OCTILE_MOTION = 'type octile'
+HEIGHT = 'height'
+WIDTH = 'width'
+MAP = 'map'
+
+class Map():
+    
+    def __init__(self):
+        self.motionModel = None
+        self.occlusion = None
+
+    @staticmethod
+    def loadMap(path):
+        with open(path, 'r') as stream:
+            lines = stream.readlines()
+            lines = [line[:-1] for line in lines]
+            newMap = Map()
+            if lines[0] == 'type octile':
+                newMap.motionModel = DiagonalMovement
+            else:
+                print("Saw unexpected motion model:", lines[0])
+                raise ValueError
+            print(lines[0:4])
+            
+
 if __name__ == "__main__":
-    print("Hello World")
+    Map.loadMap(TEST_PATH)
