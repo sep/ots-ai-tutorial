c7aafa708685017c57b15825527a22d4f9c29713
Jordan Thayer <jtthayer@sep.com>
2024-01-02 14:50:13 -0500

    Added missing configuration file

---
+++ configMachine.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Commands used to configure the VM to distribute to students Should
+# also allow students to configure their own machines automatically
+# and support building a remote box for them to connect to on digital
+# ocean or similar
+
+# Grab some standard development tools
+sudo apt-get install emacs git screen python3 python3-pip python3-venv
+# Tools for the ML tutorials
+sudo apt-get install python3-matplotlib python3-sklearn python3-pytorch python3-torchvision
+# Tools for the NLP tutorials
+
+
